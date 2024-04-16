53d6c872e886eb741d0997d0f5091762cd6749a5
Glaucco Siqueira <113634929+glauccoslima@users.noreply.github.com>
2024-04-16 19:16:07 -0300

    improve

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// Define a type for the Vite configuration object
+interface IViteConfig {
+  plugins?: react[];
+}
+
+// Export a default function that returns the Vite configuration object
+export default (): defineConfig => {
+  return {
+    // Give the configuration object a more descriptive name
+    reactPlugins: [react()],
+  } as IViteConfig;
+};
